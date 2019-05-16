4264a3af77c3dd0ef884b7406c1a5358df9f7640
Hanna Jeong <id0242han@naver.com>
2019-05-16 11:58:19 +0900

    #2 텍스트발스,버튼,라벨 컨트롤의 이벤트코드 구현

--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -11,9 +11,45 @@ namespace StringIO_TextView
 {
     public partial class Form1 : Form
     {
+        private string OrgStr;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblResult.Text;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
+        }
+
+        private bool TextCheck()
+        {
+            if(this.txtEdit.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("텍스트를 입력하세요!", "알림" ,MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return false;
+
+            }
+        }
+        
+        private void txtEdit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)13){
+                if (TextCheck())
+                {
+                    this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
+                }
+            }
         }
     }
 }
